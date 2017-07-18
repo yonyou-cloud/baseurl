c7c58a63701dc5a04c29f1629c4a627612320355
薛定谔的猫 <hh_2013@foxmail.com>
2017-07-18 20:01:39 +0100

    init

---
+++ index.js
@@ -0,0 +1,30 @@
+// 根据项目类型和后端（php/java）获取相应后端域名
+var urls = {
+  'web': {
+    'php': {
+      'test': '//web.api.chaoke.com',
+      'pre': '//web-api.esn.ren',
+      'prod': '//web-api.yonyoucloud.com'
+    },
+    'java': {
+      'test': '//123.103.9.195:6680',
+      'pre': '//123.103.9.195:6680',
+      'prod': '//ezone.yonyoucloud.com'
+    }
+  },
+  'h5': {
+    'php': {
+      'test': '//h5.api.chaoke.com:6061',
+      'pre': '//h5-api.esn.ren',
+      'prod': '//h5-api.yonyoucloud.com'
+    },
+    'java': {
+      'test': '//123.103.9.195:6680',
+      'pre': '//123.103.9.195:6680',
+      'prod': '//ezone.yonyoucloud.com'
+    }
+  }
+}
+module.exports = function getBaseurl(projType, lang, env) {
+  return urls[projType][lang][env];
+}
---
+++ readme.md
@@ -0,0 +1,19 @@
+# @yonyou/baseurl
+
+## Install
+
+```bash
+npm i yonyou-cloud/baseurl
+```
+
+## Usage
+
+```js
+var baseurl = require('@yonyou-cloud/baseurl')('web', 'php', 'prod'); // 返回"//web-api.yonyoucloud.com"
+```
+
+## Params
+
++ projType: `web` | `h5`
++ lang: `php` | `java`
++ env: `test` | `pre` | `prod`
